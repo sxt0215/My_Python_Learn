5dd39d52f6764b57c6820db807e44e0142cb5836
DESKTOP-PQFCG6K\sxt_0 <sxt_0215@163.com>
2019-06-10 10:17:42 +0800

    no message

---
+++ list_or_tuple.py
@@ -0,0 +1,73 @@
+classmates = ['Michael','Bob','Tracy']
+print(classmates)
+print(len(classmates))
+print(classmates[1])
+classmates.append('Adam')
+print(classmates)
+classmates.insert(1,'Jack')
+print(classmates)
+classmates.pop()
+print(classmates)
+classmates.pop(1)
+print(classmates)
+classmates[1] = 'Sarah'
+print(classmates)
+L = ['Apple',123,True]
+print(L)
+s = ['python','java',['asp','php'],'scheme']
+print(s)
+print(len(s))
+p = ['asp','php']
+y = ['qwe','asd',p,'zxc']
+print(p)
+print(y)
+L = []
+print(len(L))
+
+
+
+t = (1,2)
+print(t)
+null = 0
+print(null)
+one = (1)
+print(one)
+bian = ('qaz','wsx',['edc','rfv'])
+print(bian)
+bian[2][0] = 'A'
+bian[2][1] = 'B'
+print(bian)
+
+
+Test = [
+		['Apple','Google','BaiDu'],
+		['Java','Python','Ruby'],
+		['Mary','Tom','Cat','Lisa']
+]
+
+print(Test[0][0])
+print(Test[1][1])
+print(Test[2][3])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tiaojian.py
@@ -0,0 +1,7 @@
+age = 2
+if age >= 18:
+	print('your age is',age)
+	print('adult')
+else:
+	print('your age id',age)
+	print('teenager')
